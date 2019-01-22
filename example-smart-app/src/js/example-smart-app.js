a1f72f20d0a3e579241b8072053bf87f8d51dd72
kjacks89 <kjacks89@gmail.com>
2019-01-22 15:48:09 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,7 +10,7 @@
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
-        document.write(patient);
+        document.write(JSON.stringify(patient));
         /*
         function getOrders(patient) {
           var xmlhttp = new XMLHttpRequest();
